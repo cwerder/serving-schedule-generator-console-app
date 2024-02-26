afb417a88c5853ff4eab9b3fa265d8cd83db0182
CJ Werder <cwerder15@gmail.com>
2024-02-26 18:29:09 -0500

    fix: date ranges now show up properly as inputted

--- DTOs/MassDay.py
+++ DTOs/MassDay.py
@@ -7,8 +7,29 @@ class MassDay:
 		self.dayATCFormat = dayATCFormat
 		self.masses = [Mass(**mass_data) for mass_data in masses]
 
-	def __repr__(self):
-		return self.day
+	def __str__(self, indent = 0):
+		result = ""
+		for key, value in vars(self).items():
+			if isinstance(value, MassDay):
+				result += " " * indent + f"{key}:\n"
+				result += value.__str__(indent + 4)
+			elif isinstance(value, (list, tuple, set)):
+				result += " " * indent + f"{key}:\n"
+				for item in value:
+					if isinstance(item, MassDay):
+						result += item.__str__(indent + 4)
+					else:
+						result += " " * (indent + 4) + str(item) + "\n"
+			elif isinstance(value, dict):
+				result += " " * indent + f"{key}:\n"
+				for k, v in value.items():
+					if isinstance(v, MassDay):
+						result += v.__str__(indent + 4)
+					else:
+						result += " " * (indent + 4) + f"{k}: {v}\n"
+			else:
+				result += " " * indent + f"{key}: {value}\n"
+		return result
 
 
 class Mass:
@@ -17,3 +38,27 @@ class Mass:
 		self.timeATC = timeATC
 		self.description = description
 		self.skip = skip
+
+	def __str__(self, indent=0):
+		result = ""
+		for key, value in vars(self).items():
+			if isinstance(value, Mass):
+				result += " " * indent + f"{key}:\n"
+				result += value.__str__(indent + 4)
+			elif isinstance(value, (list, tuple, set)):
+				result += " " * indent + f"{key}:\n"
+				for item in value:
+					if isinstance(item, Mass):
+						result += item.__str__(indent + 4)
+					else:
+						result += " " * (indent + 4) + str(item) + "\n"
+			elif isinstance(value, dict):
+				result += " " * indent + f"{key}:\n"
+				for k, v in value.items():
+					if isinstance(v, Mass):
+						result += v.__str__(indent + 4)
+					else:
+						result += " " * (indent + 4) + f"{k}: {v}\n"
+			else:
+				result += " " * indent + f"{key}: {value}\n"
+		return result
